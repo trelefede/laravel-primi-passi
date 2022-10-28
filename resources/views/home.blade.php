ac856f6300b2b20bbbb27c26659ba904b8949f11
TreleaniFederico <treleani.federico@gmail.com>
2022-10-28 15:51:39 +0200

    ad data array add foreach to print array

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,34 @@
 <h1>HELLO WORLD!!</h1>
+
+@php
+    $array = [
+        [
+            'misura' => '205/55/16',
+            'marca' => 'Hankook',
+            'modello' => 'W452',
+        ],
+        [
+            'misura' => '205/55/16',
+            'marca' => 'Continental',
+            'modello' => 'TS860',
+        ],
+        [
+            'misura' => '205/55/16',
+            'marca' => 'Pirelli',
+            'modello' => 'SottoZero 3',
+        ],
+    ];
+
+@endphp
+
+<h1>Pneumatici Invernali</h1>
+
+<div>
+    @foreach ($array as $data)
+        <div>
+            <p> {{ $data['marca'] }} </p>
+            <p> {{ $data['modello'] }} </p>
+            <p> {{ $data['misura'] }} </p>
+        </div>
+    @endforeach
+</div>
